dc805815d00d096547a5a1edf7d6437992011770
el_loco <jonas_waechter02@mail.de>
2025-05-11 20:37:39 +0200

    added player death screen

--- wildbeat/scripts/globals/game_manager.gd
+++ wildbeat/scripts/globals/game_manager.gd
@@ -1,12 +1,19 @@
 extends Node
 
+var time_alive := 0.0
+var is_alive := true
 
-#
-#func _input(event):
-	#if event.is_action_pressed("pause_game"):
-		#if get_tree().paused:
-			#pause_menu.hide_pause_menu()
-			#print("currently pause resumning game")
-		#else:
-			#pause_menu.show_pause_menu()
-			#print("pausing game")
+func _process(delta):
+	if is_alive:
+		time_alive += delta
+
+func display_time():
+	is_alive = false
+	var root = get_tree().get_current_scene()
+	print("root:" + str(root))
+	var label = get_tree().current_scene.get_node("Label")
+	if label:
+		label.text = "Du hast " + str("%0.2f" % time_alive)  + " Sekunden überlebt"
+	print("time alive:" + str(time_alive))
+
+	
