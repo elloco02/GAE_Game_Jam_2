44df15b8074fc195e23228455e033004fd1dae19
Eren Saglam <2120548@stud.hs-mannheim.de>
2025-05-17 17:30:24 +0200

    Added a scoreboard system

--- wildbeat/scripts/globals/game_manager.gd
+++ wildbeat/scripts/globals/game_manager.gd
@@ -1,20 +1,9 @@
 extends Node
 
-var time_alive := 0.0
-var is_alive := true
-
-func _process(delta):
-	if is_alive:
-		time_alive += delta
-
 func display_time():
-	is_alive = false
 	var root = get_tree().get_current_scene()
 	print("root:" + str(root))
-	var label = get_tree().current_scene.get_node("Label")
-	if label:
-		label.text = "Du hast " + str("%0.2f" % time_alive)  + " Sekunden überlebt"
-	print("time alive:" + str(time_alive))
-	ScoreManager.score = 0
-
+	var scoreLabel = get_tree().current_scene.get_node("ScoreLabel")
+	if scoreLabel:
+		scoreLabel.text = "Your score: " + str(ScoreManager.score)
 	
