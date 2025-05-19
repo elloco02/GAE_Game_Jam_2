c8641b875ca82ecbe65d2ced4b9db0cebce37885
el_loco <jonas_waechter02@mail.de>
2025-05-19 22:59:22 +0200

    testing sounds

--- wildbeat/scripts/falling_objects/fallable_shield.gd
+++ wildbeat/scripts/falling_objects/fallable_shield.gd
@@ -6,6 +6,7 @@ extends Fallable
 
 # player receives a shield that protects from damage
 func on_player_entered(player: Player) -> void:
+	AudioManager.create_2d_audio_at_location(global_position, SoundEffectSettings.SOUND_EFFECT_TYPE.PLAYER_SHIELDING)
 	player.activate_shield()
 	print("shield collected")
 	queue_free() # Default behavior is to remove the object
